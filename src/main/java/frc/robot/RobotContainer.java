d380975975b5047a9fd1d2e58c486bc0390fec86
Gage Rocheleau <gager198@gmail.com>
2022-02-24 12:04:30 -0500

    full blueberry tankdrive robot (should be working)

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.nio.file.DirectoryIteratorException;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+//import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.OIConstants; //constants
+import frc.robot.commands.TankDrive; //drive command
+import frc.robot.subsystems.Drivetrain; // drivetrain subsystem
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  public final Drivetrain m_drive = new Drivetrain();
+  public final TankDrive m_teleopCommand = new TankDrive(null, null, m_drive); // (teleop command)
+  
+  //Joystick
+  public final Joystick m_stick = new Joystick(OIConstants.kJoystick);
+  public final XboxController m_xboxStick = new XboxController(OIConstants.kXboxController);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+
+    m_drive.setDefaultCommand(new TankDrive(() -> -m_xboxStick.getLeftY(), () -> m_xboxStick.getRightX(), m_drive));
+
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {}
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return new WaitCommand(15); //does nothing
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,85 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants; //constant
+
+import java.util.Map;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP; //motors
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+
+public class Drivetrain extends SubsystemBase {
+  ///// Motors
+  private static final VictorSP m_frontLeftMotor = new VictorSP(DriveConstants.kFrontLeftMotor);
+  private static final VictorSP m_rearLeftMotor = new VictorSP(DriveConstants.kRearLeftMotor);
+  private static final VictorSP m_frontRightMotor = new VictorSP(DriveConstants.kFrontRightMotor);
+  private static final VictorSP m_rearRightMotor = new VictorSP(DriveConstants.kRearRightMotor);
+
+  private static final MotorControllerGroup m_leftMotors = new MotorControllerGroup(m_frontLeftMotor, m_rearLeftMotor);
+  private static final MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_frontRightMotor, m_rearRightMotor);
+
+  private static final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+  /////
+
+  public static double tempSpeed;
+
+  private final ShuffleboardTab m_tab = Shuffleboard.getTab("Main");
+  private final NetworkTableEntry m_maxSpeed;
+
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    //configure stuff here
+    m_leftMotors.setInverted(false);
+    m_rightMotors.setInverted(false);
+
+    m_drive.setDeadband(.1);
+
+    m_maxSpeed = m_tab.add("Max Speed", 1.0).withPosition(2, 2).withSize(2, 1).withWidget(BuiltInWidgets.kNumberSlider).withProperties(Map.of("min", 0, "max", 1)).getEntry();
+    m_maxSpeed.setDouble(DriveConstants.kDefaultSpeed);
+  }
+
+  public void tankDrive(double m_leftMotors, double m_rightMotors){
+    m_drive.tankDrive(m_leftMotors, m_rightMotors);
+  }
+
+  public void setMaxSpeed(double maxSpeed) {
+    m_drive.setMaxOutput(maxSpeed);
+  }
+
+  public void stopDrive(){
+    m_drive.stopMotor();
+  }
+
+//drive speeds
+  public void slowDriveSpeed() {
+    tempSpeed = m_maxSpeed.getDouble(1.0);
+    m_maxSpeed.setDouble(tempSpeed * DriveConstants.kSlowDriveSpeed);
+  }
+
+  /**
+   * Returns the drive speed to the original speed.
+   */
+  public void normalDriveSpeed() {
+    m_maxSpeed.setDouble(tempSpeed);
+  }
+////////
+  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
